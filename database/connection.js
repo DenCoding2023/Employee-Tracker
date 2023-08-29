024278efefbbc0cd0796756016974f9ecced8f25
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-08-28 23:17:48 -0400

    create a connection page

---
+++ database/connection.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+
+
+// Connect to database
+const employee_db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'worker_db'
+    },
+    console.log('Connected to the database.')
+  );
+
+
+  employee_db.connect(function (err) {
+    if (err) throw err;
+  });
+  
+  module.exports = employee_db;
