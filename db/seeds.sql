61e54fa1c5e2b35274cee2ef5d0dce7524ba68f0
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-08-29 00:52:51 -0400

    update the file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,67 +1,32 @@
-INSERT INTO department
-(name)
+-- Insert data into the department table
+INSERT INTO department (name)
 VALUES
-('Sales'),
-('Engineering'),
-('Finance'),
-('Legal'),
-('language'),
-('administrative');
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal');
+ 
 
-INSERT INTO role
-(title, salary, department_id, age)
+-- Insert data into the role table
+INSERT INTO role (title, salary, department_id)
 VALUES
-('Sales Lead', 100000, 1, 22), 
-('Salesperson', 80000, 1, 41), 
-('Lead Engineer', 150000, 2, 65),
-('Software Engineer', 120000, 2, 32),
-('Account Manager', 160000, 3, 29),
-('Accountant', 125000, 3, 46 ),
-('Legal Team Lead', 250000, 4, 44),
-('Lawyer', 190000, 4, 45);
+  ('Sales Lead', 100000, 1),
+  ('Salesperson', 80000, 1),
+  ('Lead Engineer', 150000, 2),
+  ('Software Engineer', 120000, 2),
+  ('Account Manager', 160000, 3),
+  ('Accountant', 125000, 3),
+  ('Legal Team Lead', 250000, 4),
+  ('Lawyer', 190000, 4);
 
-
-
-INSERT INTO employee
-(first_name, last_name, role_id, manager_id)
+-- Insert data into the employee table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('John', 'Doe', 1, NULL),
-('Mike', 'Chan', 2, 1),
-('Ashley', 'Rodriguez', 3, NULL),
-('Kevin', 'Tupik', 4, 3),INSERT INTO department
-(name)
-VALUES
-('Sales'),
-('Engineering'),
-('Finance'),
-('Legal');
-
-INSERT INTO role
-(title, salary, department_id)
-VALUES
-('Sales Lead', 100000, 1),
-('Salesperson', 80000, 1),
-('Lead Engineer', 150000, 2),
-('Software Engineer', 120000, 2),
-('Account Manager', 160000, 3),
-('Accountant', 125000, 3),
-('Legal Team Lead', 250000, 4),
-('Lawyer', 190000, 4);
-
-
-
-INSERT INTO employee
-(first_name, last_name, role_id, manager_id)
-VALUES
-('John', 'Doe', 1, NULL),
-('Mike', 'Chan', 2, 1),
-('Ashley', 'Rodriguez', 3, NULL),
-('Kevin', 'Tupik', 4, 3),
-('Kunal', 'Singh', 5, NULL),
-('Malia', 'Brown', 6, 5),
-('Sarah', 'Lourd', 7, NULL),
-('Tom', 'Allen', 8, 7);
-('Kunal', 'Singh', 5, NULL),
-('Malia', 'Brown', 6, 5),
-('Sarah', 'Lourd', 7, NULL),
-('Tom', 'Allen', 8, 7);
+  ('John', 'Doe', 1, NULL),
+  ('Mike', 'Chan', 2, 1),
+  ('Ashley', 'Rodriguez', 3, NULL),
+  ('Kevin', 'Tupik', 4, 3),
+  ('Kunal', 'Singh', 5, NULL),
+  ('Malia', 'Brown', 6, 5),
+  ('Sarah', 'Lourd', 7, NULL),
+  ('Tom', 'Allen', 8, 7);
